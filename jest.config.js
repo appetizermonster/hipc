3da6fdcb64835c40e00273f6b35cbb712c30d9ee
Heejin Lee <hjhome2000@naver.com>
2017-11-13 17:16:43 +0900

    Update jest configuration
    - Set coverage path patterns
    - Formatting

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest/preprocessor.js'
   },
-  testRegex: '\.\/test\/.+\.spec\.tsx?',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json']
+  testRegex: './test/.+.spec.tsx?',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/test',
+    '<rootDir>/examples'
+  ]
 };
