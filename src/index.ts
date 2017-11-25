3f8fece631c91ea520bc90fadb61eaea669271cb
Heejin Lee <hjhome2000@naver.com>
2017-11-26 01:25:18 +0900

    Fix compile error

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import IpcClient, { IIpcClientOptions } from './IpcClient';
 import IpcServer from './IpcServer';
-import IpcServiceRegistry from './IpcServiceRegistry';
 import { IChannelClient, IChannelServer } from './types';
 
-export const createServiceRegistry = () => new IpcServiceRegistry();
 export const createServer = (channel: IChannelServer) => new IpcServer(channel);
 export const createClient = (
   channel: IChannelClient,
