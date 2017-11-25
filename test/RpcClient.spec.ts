152b23f5927bc415f21e1f51ab1502e0b2dfcd97
Heejin Lee <hjhome2000@naver.com>
2017-11-25 23:51:40 +0900

    Add test for RpcClient

---
+++ test/RpcClient.spec.ts
@@ -0,0 +1,43 @@
+import RpcClient from '../src/RpcClient';
+import { IChannelClient, IRpcService } from '../src/types';
+
+interface IDummyService extends IRpcService {
+  dummyFunc(): void;
+}
+
+function createMockChannel(): IChannelClient {
+  return ({
+    connect: jest.fn(() => Promise.resolve())
+  } as any) as IChannelClient;
+}
+
+describe('RpcClient', () => {
+  describe('connect', () => {
+    it('should call `connect` on the channel', async () => {
+      const mockChannel = createMockChannel();
+      const client = new RpcClient(mockChannel);
+      await client.connect();
+      expect(mockChannel.connect).toBeCalled();
+    });
+  });
+
+  describe('getServiceProxy', () => {
+    it('should throw error if not connected', () => {
+      const mockChannel = createMockChannel();
+      const client = new RpcClient(mockChannel);
+      const fn = () => client.getServiceProxy<IDummyService>('dummyService');
+      expect(fn).toThrowError();
+    });
+
+    it('should return serviceProxy', async () => {
+      const mockChannel = createMockChannel();
+      const client = new RpcClient(mockChannel);
+      await client.connect();
+
+      const dummyService = client.getServiceProxy<IDummyService>(
+        'dummyService'
+      );
+      expect(dummyService).toBeDefined();
+    });
+  });
+});
